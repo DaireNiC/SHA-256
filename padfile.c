2184d85ed168eac73c0943101dad8132de678960
Daire <dairenichat@gmail.com>
2019-03-27 13:35:58 +0000

    added padding for when < 55 bytes

--- padfile.c
+++ padfile.c
@@ -11,30 +11,41 @@ union msgblock {
 };
 
 int main(int argc, char *argv[]){
-
   union msgblock M;
-  
   uint64_t nobytes;
+  uint64_t nobits = 0;
 
   FILE* f;
   char c;
-
   // open file in read mode
   f = fopen(argv[1], "r");
-
   //deals in bytes -->  1 = 1 byte
   // one byte at a time
- // printf("%c\n", fread(&c, 1, 1, f));
-
   while(!feof(f)){
     nobytes = fread(M.e, 1, 64, f);
-    printf("%11u\n", nobytes);
-
+    printf("Read %211u\n bytes\n", nobytes);
+    //keeps track of all bits read
+    nobits = nobits +(nobytes * 8);
+    if(nobytes < 56){
+      printf("I've found a block with less htan 55 bytes!\n");
+      M.e[nobytes] = 0x80; //1 in hex
+      while(nobytes < 56){
+	//index in the block
+        nobytes = nobytes + 1;
+	//set all the bytes to 0 
+        M.e[nobytes] = 0x00;
+      }
+     //access using union
+      M.s[7] = nobits;
+    }
   }
-
   fclose(f);
 
-  return 0;
+  for (int i = 0; i < 64; i++) {
+    printf("%x ", M.e[i]);
+    printf("\n");
+  }
 
+  return 0;
 }
 
