ae5e66236f411ad883def27f901150394316fe7b
Daire <dairenichat@gmail.com>
2019-03-28 00:08:15 +0000

    started work on final padding part

--- padfile.c
+++ padfile.c
@@ -10,42 +10,51 @@ union msgblock {
   uint64_t s[8];
 };
 
+
+// flags for keeping track of status when reading file
+enum status {READ, PAD0, PAD1, FINISH};
+
 int main(int argc, char *argv[]){
+
+
   union msgblock M;
   uint64_t nobytes;
   uint64_t nobits = 0;
 
+  // pointer for file
   FILE* f;
-  char c;
+
   // open file in read mode
   f = fopen(argv[1], "r");
   //deals in bytes -->  1 = 1 byte
   // one byte at a time
   while(!feof(f)){
+    // Read in file 64 bytes at a time
     nobytes = fread(M.e, 1, 64, f);
+
     printf("Read %211u\n bytes\n", nobytes);
-    //keeps track of all bits read
     nobits = nobits +(nobytes * 8);
+
+    // if msg block has < 56 byte ---> pad it out with 0s
     if(nobytes < 56){
       printf("I've found a block with less htan 55 bytes!\n");
-      M.e[nobytes] = 0x80; //1 in hex
+      M.e[nobytes] = 0x80; //1 in hex (as per spec)
       while(nobytes < 56){
-	//index in the block
         nobytes = nobytes + 1;
-	//set all the bytes to 0 
+         // 0 in hex for padding
         M.e[nobytes] = 0x00;
       }
-     //access using union
+
+      // savea the num of bits in the entire message
       M.s[7] = nobits;
     }
   }
   fclose(f);
-// print to check output
+
   for (int i = 0; i < 64; i++) {
     printf("%x ", M.e[i]);
-    printf("\n");
+    print("\n");
   }
 
   return 0;
 }
-
